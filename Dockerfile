1bc438616b677311ad87e554fdbee4f059d630ab
t-thanh <nttputus@gmail.com>
2021-07-30 17:23:09 +0200

    keep original - add Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM turlucode/ros-melodic:cuda10.1-cudnn7
+FROM mzahana/px4-ros-melodic-cuda10.1:latest
 
 MAINTAINER t-thanh <tien.thanh@eu4m.eu>
 
-RUN apt-get update && apt-get install -y sudo wget
+USER root
+RUN apt-get update && sudo apt-get install -y sudo wget terminator
 RUN adduser --disabled-password --gecos '' docker
 RUN adduser docker sudo
 RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
@@ -16,8 +17,38 @@ RUN chown ${uid}:${gid} -R /home/docker
 
 USER docker
 WORKDIR /home/docker
-RUN /bin/bash -c 'sudo apt-get update && sudo apt-get install -y git && source /opt/ros/melodic/setup.bash && \
-	cd ~/ && git clone https://github.com/t-thanh/px4_fast_planner && cd px4_fast_planner && ./setup.sh'
-RUN echo "source ~/px4_fast_planner/setup-px4.sh" > ~/.bashrc
+RUN mkdir -p /home/docker/.config/terminator/config
+RUN /bin/bash -c 'source /opt/ros/melodic/setup.bash && \
+	mkdir -p /home/docker/catkin_ws/src && cd /home/docker/catkin_ws && catkin_make && \
+	source /home/docker/catkin_ws/devel/setup.bash && \
+	echo "source /home/docker/catkin_ws/devel/setup.bash" >> /home/docker/setup-px4.sh && \
+	sudo apt --quiet -y install ca-certificates gnupg lsb-core wget && \
+	cd /home/docker/catkin_ws/src && git clone https://github.com/mzahana/px4_fast_planner && \
+	git clone https://github.com/Jaeyoung-Lim/mavros_controllers.git && \
+	git clone https://github.com/catkin/catkin_simple && \
+	git clone https://github.com/ethz-asl/eigen_catkin && \
+	git clone https://github.com/ethz-asl/mav_comm && \
+	git clone https://github.com/mzahana/Fast-Planner.git && \
+	cd Fast-Planner && git checkout changes_for_ros_melodic && \
+	sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install astyle build-essential ccache clang clang-tidy cmake cppcheck doxygen file g++ gcc gdb git lcov make ninja-build python3 python3-dev python3-pip python3-setuptools python3-wheel rsync shellcheck unzip xsltproc zip && \
+	pip3 install --user -r /home/docker/catkin_ws/src/px4_fast_planner/install/px4_requirements.txt && \
+	sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libeigen3-dev libgazebo9-dev libgstreamer-plugins-base1.0-dev libimage-exiftool-perl libopencv-dev libxml2-utils pkg-config protobuf-compiler libfreeimage-dev'
+RUN cd /home/docker && git clone https://github.com/PX4/Firmware && cd Firmware && make clean && make distclean && \
+	git checkout v1.10.1 && git submodule init && git submodule update --recursive && \
+	cd /home/docker/Firmware/Tools/sitl_gazebo/external/OpticalFlow && git submodule init && git submodule update --recursive && \
+	cd /home/docker/Firmware/Tools/sitl_gazebo/external/OpticalFlow/external/klt_feature_tracker && \
+	git submodule init && git submodule update --recursive && \
+	sed -i 's/#define HAS_GYRO.*/#define HAS_GYRO true/' /home/docker/Firmware/Tools/sitl_gazebo/include/gazebo_opticalflow_plugin.h && \
+	cd /home/docker/Firmware && DONT_RUN=1 make px4_sitl gazebo
+RUN /bin/bash -c 'source /home/docker/catkin_ws/devel/setup.bash && \
+	echo "source /home/docker/Firmware/Tools/setup_gazebo.bash /home/docker/Firmware $HOME/Firmware/build/px4_sitl_default" >> /home/docker/setup-px4.sh && \
+	echo "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/docker/Firmware/Tools/sitl_gazebo::/home/docker/Firmware" >> /home/docker/setup-px4.sh && \
+	echo "export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins" >> /home/docker/setup-px4.sh && \
+	echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/docker/catkin_ws/src/px4_fast_planner/models" >> /home/docker/setup-px4.sh && \
+	cp /home/docker/catkin_ws/src/px4_fast_planner/config/10017_iris_depth_camera /home/docker/Firmware/ROMFS/px4fmu_common/init.d-posix/ && \
+	source /home/docker/setup-px4.sh && \
+	sudo apt install ros-melodic-mavros ros-melodic-mavros-extras ros-melodic-nlopt libarmadillo-dev -y && \
+	cd /home/docker/catkin_ws && catkin_make'
+RUN echo "source /home/docker/setup-px4.sh" > ~/.bashrc
 # Launch terminator
-CMD ["terminator"]
+CMD terminator -u
--- setup.sh
+++ setup.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-BUILD_PX4="true"
-
-#echo -e "\e[1;33m Do you want to build PX4 v1.10.1? (y) for simulation (n) if setting this up on on-board computer \e[0m"
-#read var
-#if [ "$var" != "y" ] && [ "$var" != "Y" ] ; then
-#    echo -e "\e[1;33m Skipping PX4 v1.10.1 \e[0m"
-#    BUILD_PX4="false"
-#    sleep 1
-#else
-#    echo -e "\e[1;33m PX4 v1.10.1 will be built \e[0m"
-#    BUILD_PX4="true"
-#    sleep 1
-#fi
-
-echo -e "\e[1;33m PX4 v1.10.1 will be built \e[0m"
-
-
 CATKIN_WS=$PWD/catkin_ws
 CATKIN_SRC=$CATKIN_WS/src
 HOMEDIR=$PWD
@@ -32,122 +15,114 @@ fi
 
 # Configure catkin_Ws
 cd $CATKIN_WS
+sudo apt --quiet -y install python-catkin-tools 
 catkin init
 catkin config --merge-devel
 catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release
 
-grep -xF 'source $CATKIN_WS/devel/setup.bash' $HOMEDIR/setup-px4.sh || echo "source $CATKIN_WS/devel/setup.bash" >> $HOMEDIR/setup-px4.sh
+echo "source $CATKIN_WS/devel/setup.bash" >> $HOMEDIR/setup-px4.sh
 
 ####################################### Setup PX4 v1.10.1 #######################################
-if [ "$BUILD_PX4" != "false" ]; then
+echo -e "\e[1;33m Setting up Px4 v1.10.1 \e[0m"
+# Installing initial dependencies
+sudo apt --quiet -y install \
+	ca-certificates \
+	gnupg \
+	lsb-core \
+	wget \
+	;
+# script directory
+cd ${CATKIN_SRC}/px4_fast_planner/install
 
-    echo -e "\e[1;33m Setting up Px4 v1.10.1 \e[0m"
-    # Installing initial dependencies
-    sudo apt --quiet -y install \
-        ca-certificates \
-        gnupg \
-        lsb-core \
-        wget \
-        ;
-    # script directory
-    cd ${CATKIN_SRC}/px4_fast_planner/install
-    DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "Installing PX4 general dependencies"
 
-    # check requirements.txt exists (script not run in source tree)
-    REQUIREMENTS_FILE="px4_requirements.txt"
-    if [ ! -f "${DIR}/${REQUIREMENTS_FILE}" ]; then
-        echo "FAILED: ${REQUIREMENTS_FILE} needed in same directory as setup.sh (${DIR})."
-        return 1
-    fi
+sudo apt-get update -y --quiet
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
+	astyle \
+	build-essential \
+	ccache \
+	clang \
+	clang-tidy \
+	cmake \
+	cppcheck \
+	doxygen \
+	file \
+	g++ \
+	gcc \
+	gdb \
+	git \
+	lcov \
+	make \
+	ninja-build \
+	python3 \
+	python3-dev \
+	python3-pip \
+	python3-setuptools \
+	python3-wheel \
+	rsync \
+	shellcheck \
+	unzip \
+	xsltproc \
+	zip \
+	;
+	
+# Python3 dependencies
+echo
+echo "Installing PX4 Python3 dependencies"
+pip3 install --user -r $CATKIN_SRC/px4_fast_planner/install/px4_requirements.txt
+wget https://raw.githubusercontent.com/PX4/Firmware/master/Tools/setup/requirements.txt
+pip install --user -r $CATKIN_SRC/px4_fast_planner/install/requirements.txt
 
-    echo "Installing PX4 general dependencies"
-
-    sudo apt-get update -y --quiet
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
-        astyle \
-        build-essential \
-        ccache \
-        clang \
-        clang-tidy \
-        cmake \
-        cppcheck \
-        doxygen \
-        file \
-        g++ \
-        gcc \
-        gdb \
-        git \
-        lcov \
-        make \
-        ninja-build \
-        python3 \
-        python3-dev \
-        python3-pip \
-        python3-setuptools \
-        python3-wheel \
-        rsync \
-        shellcheck \
-        unzip \
-        xsltproc \
-        zip \
-        ;
-        
-    # Python3 dependencies
-    echo
-    echo "Installing PX4 Python3 dependencies"
-    pip3 install --user -r $CATKIN_SRC/px4_fast_planner/install/px4_requirements.txt
-
-    echo "arrow" | sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
-            gstreamer1.0-plugins-bad \
-            gstreamer1.0-plugins-base \
-            gstreamer1.0-plugins-good \
-            gstreamer1.0-plugins-ugly \
-            libeigen3-dev \
-            libgazebo9-dev \
-            libgstreamer-plugins-base1.0-dev \
-            libimage-exiftool-perl \
-            libopencv-dev \
-            libxml2-utils \
-            pkg-config \
-            protobuf-compiler \
-            libfreeimage-dev \
-            ;
+sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install \
+		gstreamer1.0-plugins-bad \
+		gstreamer1.0-plugins-base \
+		gstreamer1.0-plugins-good \
+		gstreamer1.0-plugins-ugly \
+		libeigen3-dev \
+		libgazebo9-dev \
+		libgstreamer-plugins-base1.0-dev \
+		libimage-exiftool-perl \
+		libopencv-dev \
+		libxml2-utils \
+		pkg-config \
+		protobuf-compiler \
+		libfreeimage-dev \
+		;
 
 
-    #Setting up PX4 Firmware
-    if [ ! -d "$HOMEDIR/Firmware" ]; then
-        cd $HOMEDIR
-        git clone https://github.com/PX4/Firmware
-    else
-        echo "Firmware already exists. Just pulling latest upstream...."
-        cd $HOMEDIR/Firmware
-        git pull
-    fi
-    cd $HOMEDIR/Firmware
-    make clean && make distclean
-    git checkout v1.10.1 && git submodule init && git submodule update --recursive
-    cd $HOMEDIR/Firmware/Tools/sitl_gazebo/external/OpticalFlow
-    git submodule init && git submodule update --recursive
-    cd $HOMEDIR/Firmware/Tools/sitl_gazebo/external/OpticalFlow/external/klt_feature_tracker
-    git submodule init && git submodule update --recursive
-    # NOTE: in PX4 v1.10.1, there is a bug in Firmware/Tools/sitl_gazebo/include/gazebo_opticalflow_plugin.h:43:18
-    # #define HAS_GYRO TRUE needs to be replaced by #define HAS_GYRO true
-    sed -i 's/#define HAS_GYRO.*/#define HAS_GYRO true/' $HOMEDIR/Firmware/Tools/sitl_gazebo/include/gazebo_opticalflow_plugin.h
-    cd $HOMEDIR/Firmware
-    DONT_RUN=1 make px4_sitl gazebo
-
-    #Copying this to  setup-px4.sh file
-    grep -xF 'source $HOMEDIR/Firmware/Tools/setup_gazebo.bash $HOMEDIR/Firmware $HOMEDIR/Firmware/build/px4_sitl_default' $HOMEDIR/setup-px4.sh || echo "source $HOMEDIR/Firmware/Tools/setup_gazebo.bash $HOMEDIR/Firmware $HOMEDIR/Firmware/build/px4_sitl_default" >> $HOMEDIR/setup-px4.sh
-    grep -xF 'export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$HOMEDIR/Firmware' $HOMEDIR/setup-px4.sh || echo "export ROS_PACKAGE_PATH=\$ROS_PACKAGE_PATH:$HOMEDIR/Firmware" >> $HOMEDIR/setup-px4.sh
-    grep -xF 'export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$HOMEDIR/Firmware/Tools/sitl_gazebo' $HOMEDIR/setup-px4.sh || echo "export ROS_PACKAGE_PATH=\$ROS_PACKAGE_PATH:$HOMEDIR/Firmware/Tools/sitl_gazebo" >> $HOMEDIR/setup-px4.sh
-    grep -xF 'export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins' $HOMEDIR/setup-px4.sh || echo "export GAZEBO_PLUGIN_PATH=\$GAZEBO_PLUGIN_PATH:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins" >> $HOMEDIR/setup-px4.sh
-    grep -xF 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:'$HOMEDIR'/catkin_ws/src/px4_fast_planner/models' $HOMEDIR/setup-px4.sh || echo "export GAZEBO_MODEL_PATH=\$GAZEBO_MODEL_PATH:$CATKIN_WS/src/px4_fast_planner/models" >> $HOMEDIR/setup-px4.sh
-
-    # Copy PX4 SITL param file
-    cp $CATKIN_SRC/px4_fast_planner/config/10017_iris_depth_camera $HOMEDIR/Firmware/ROMFS/px4fmu_common/init.d-posix/
-
-    source $HOMEDIR/setup-px4.sh
+#Setting up PX4 Firmware
+if [ ! -d "$HOMEDIR/Firmware" ]; then
+	cd $HOMEDIR
+	git clone https://github.com/PX4/Firmware
+else
+	echo "Firmware already exists. Just pulling latest upstream...."
+	cd $HOMEDIR/Firmware
+	git pull
 fi
+cd $HOMEDIR/Firmware
+make clean && make distclean
+git checkout v1.10.1 && git submodule init && git submodule update --recursive
+cd $HOMEDIR/Firmware/Tools/sitl_gazebo/external/OpticalFlow
+git submodule init && git submodule update --recursive
+cd $HOMEDIR/Firmware/Tools/sitl_gazebo/external/OpticalFlow/external/klt_feature_tracker
+git submodule init && git submodule update --recursive
+# NOTE: in PX4 v1.10.1, there is a bug in Firmware/Tools/sitl_gazebo/include/gazebo_opticalflow_plugin.h:43:18
+# #define HAS_GYRO TRUE needs to be replaced by #define HAS_GYRO true
+sed -i 's/#define HAS_GYRO.*/#define HAS_GYRO true/' $HOMEDIR/Firmware/Tools/sitl_gazebo/include/gazebo_opticalflow_plugin.h
+cd $HOMEDIR/Firmware
+DONT_RUN=1 make px4_sitl gazebo
+
+#Copying this to  setup-px4.sh file
+grep -xF 'source $HOMEDIR/Firmware/Tools/setup_gazebo.bash $HOMEDIR/Firmware $HOMEDIR/Firmware/build/px4_sitl_default' $HOMEDIR/setup-px4.sh || echo "source $HOMEDIR/Firmware/Tools/setup_gazebo.bash $HOMEDIR/Firmware $HOMEDIR/Firmware/build/px4_sitl_default" >> $HOMEDIR/setup-px4.sh
+grep -xF 'export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$HOMEDIR/Firmware' $HOMEDIR/setup-px4.sh || echo "export ROS_PACKAGE_PATH=\$ROS_PACKAGE_PATH:$HOMEDIR/Firmware" >> $HOMEDIR/setup-px4.sh
+grep -xF 'export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$HOMEDIR/Firmware/Tools/sitl_gazebo' $HOMEDIR/setup-px4.sh || echo "export ROS_PACKAGE_PATH=\$ROS_PACKAGE_PATH:$HOMEDIR/Firmware/Tools/sitl_gazebo" >> $HOMEDIR/setup-px4.sh
+grep -xF 'export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins' $HOMEDIR/setup-px4.sh || echo "export GAZEBO_PLUGIN_PATH=\$GAZEBO_PLUGIN_PATH:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins" >> $HOMEDIR/setup-px4.sh
+grep -xF 'export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:'$HOMEDIR'/catkin_ws/src/px4_fast_planner/models' $HOMEDIR/setup-px4.sh || echo "export GAZEBO_MODEL_PATH=\$GAZEBO_MODEL_PATH:$CATKIN_WS/src/px4_fast_planner/models" >> $HOMEDIR/setup-px4.sh
+
+# Copy PX4 SITL param file
+cp $CATKIN_SRC/px4_fast_planner/config/10017_iris_depth_camera $HOMEDIR/Firmware/ROMFS/px4fmu_common/init.d-posix/
+
+source $HOMEDIR/setup-px4.sh
 
 
 # Install MAVROS
